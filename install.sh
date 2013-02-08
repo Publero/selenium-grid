7858070fdbcb69b8724a274cbdc6d1a83eeb4fa0
Tomáš Pecsérke <tomas.pecserke@publero.com>
2013-02-08 15:06:53 +0100

    one cmd install script

--- install.sh
+++ install.sh
@@ -1,30 +1,32 @@
 #!/bin/sh
 
+vagrant_switch="--vagrant"
+
 if [ $# -gt 1 ]; then
-  echo "Usage: $0 [ --vagrant ]" >&2
+  echo "Usage: $0 [ $vagrant_switch ]" >&2
   exit 1
 fi
 
-if [ $# -eq 1 ] && [ "$1" != "--vagrant" ]; then
-  echo "Usage: $0 [ --vagrant ]" >&2
+if [ $# -eq 1 ] && [ "$1" != "$vagrant_switch" ]; then
+  echo "Usage: $0 [ $vagrant_switch ]" >&2
   exit 1
 fi
 
 branch=master
-name=selenium
+name=selenium-grid
 repo=Publero/$name
 
 # Download installation content & prepare
 wget -O $name.tar.gz https://github.com/$repo/archive/$branch.tar.gz
 tar -xzf $name.tar.gz
 rm $name.tar.gz
-mv ${name}-${branch} $name
+mv ${name}-* $name
 
 # Install
-if [ $# -eq 1 ] && [ $1 == "--vagrant" ]; then
-  $name/vagrant-selenium-setup.sh
-else
+if [ $# -eq 0 ] || [ "$1" != "$vagrant_switch" ]; then
   sudo $name/selenium-setup.sh
+else
+  $name/vagrant-selenium-setup.sh
 fi
 
 # Clean up
